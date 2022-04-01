7c79d0ef2f71f0aac51d157688dd290ba9654c9b
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-04-01 13:36:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, i, num_0=0, num_1=1, num_2=0;
+    cin >> n;
+    if (n>=2) {
+        for (i=0; i<n; i++) {
+            num_2=num_0+num_1;
+            cout << num_2 << endl;
+            num_1=num_0;
+            num_0=num_2;
+        }
+    } else {
+        cout << "Errore" << endl;
+    }
+    return 0;
 }
